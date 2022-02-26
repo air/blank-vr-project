4c4fd59c02c8eea1b6b6658dc06821a5847bb0c2
Aaron Bell <aaron.bell@gmail.com>
2022-02-25 22:44:11 -0500

    testing working with VR emulator

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
+import { VRButton } from '/js/VRButton.js';
+
 const three = new Threestrap.Bootstrap();
 
+// export refs to Chrome console
+window.three = three;
+
+// test scene
 const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff3333 }));
-
 three.scene.add(cube);
-
 three.camera.position.set(1, 1, 2);
 three.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+// VR button
+document.body.appendChild(VRButton.createButton(three.renderer));
+// "tell your instance of WebGLRenderer to enable XR rendering"
+three.renderer.xr.enabled = true;
+// can't use window.requestAnimationFrame any more so:
+three.renderer.setAnimationLoop( function () {
+	three.renderer.render(three.scene, three.camera);
+} );
 
 three.on('update', function () {
   cube.rotateY(0.02);
